c1ebf049f625c131f9864a6a9c889625445b314e
Diego Gonzalez <diego.gonzalez.1608@gmail.com>
2020-11-06 12:45:10 +0100

    Updates

---
+++ precache-manifest.77b520379c050e959a7c66e502a61bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e28fff3849a42d12592f891fb94a98a",
+    "url": "/diegoglez098/Crambo_AV-Companion/index.html"
+  },
+  {
+    "revision": "a6763b18537dac7dc9c6",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/css/2.09a012b4.chunk.css"
+  },
+  {
+    "revision": "e4233810ae1a926d28dc",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/css/main.2899713e.chunk.css"
+  },
+  {
+    "revision": "a6763b18537dac7dc9c6",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/js/2.0ce12401.chunk.js"
+  },
+  {
+    "revision": "b2df6f42d3b318f0b6ad3605dbfe5698",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/js/2.0ce12401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4233810ae1a926d28dc",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/js/main.9c0b8e57.chunk.js"
+  },
+  {
+    "revision": "402728972300b03c29e6",
+    "url": "/diegoglez098/Crambo_AV-Companion/static/js/runtime-main.3990cf29.js"
+  }
+]);
